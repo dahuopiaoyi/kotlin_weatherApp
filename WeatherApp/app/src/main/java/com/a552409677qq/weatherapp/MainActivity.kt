084b0517d058ddb5bf33ee2680f61baa1e89c815
dahuopiaoyi <552409677@qq.com>
2017-06-02 16:25:00 +0800

    RecyclerViewTest

--- WeatherApp/app/src/main/java/com/a552409677qq/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/a552409677qq/weatherapp/MainActivity.kt
@@ -2,14 +2,32 @@ package com.a552409677qq.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+//为了可以使用anko提供的find方法
+import org.jetbrains.anko.find
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Mon 6/23 - Sunny - 31/17"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "hello kotlin"
+        val forecastList: RecyclerView = find(R.id.forecast_list)
+//        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+//        message.text = "hello kotlin"
     }
 }
